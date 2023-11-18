841f3969a8639b5edc42276fa69dc831d0e0ef09
YuriiTrofimenko <tyaa@ukr.net>
2023-11-18 10:38:35 +0300

    начальный

---
+++ src/main/java/org/tyaa/training/current/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package org.tyaa.training.current.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
