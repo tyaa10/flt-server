a5f0e5a5a0fce4b33ad9bbb0cba6cbf63d6ace38
YuriiTrofimenko <tyaa@ukr.net>
2023-11-18 15:52:30 +0300

    первая модель и первый контроллер для проверки работоспособности приложения

--- src/main/java/org/tyaa/training/current/server/ServerApplication.java
+++ src/main/java/org/tyaa/training/current/server/ServerApplication.java
@@ -3,6 +3,9 @@ package org.tyaa.training.current.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Главный класс приложения
+ * */
 @SpringBootApplication
 public class ServerApplication {
 
