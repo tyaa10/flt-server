8655b932d9d4f5fbbd985f6472a3b5f257dc93c3
YuriiTrofimenko <tyaa@ukr.net>
2023-12-23 11:40:33 +0300

    настройка соединения с реляционной базой данных postgres, первая сущность и первый репозиторий для получения web-клиентом данных из базы

---
+++ src/main/java/org/tyaa/training/current/server/repositories/RoleRepository.java
@@ -0,0 +1,13 @@
+package org.tyaa.training.current.server.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.tyaa.training.current.server.entities.RoleEntity;
+
+/**
+ * Репозиторий для работы с ролями пользователей
+ * */
+@Repository
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+
+}
