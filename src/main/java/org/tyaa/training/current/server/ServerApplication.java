bb46b27dca6cd2eeb150ad757cb98111bdc86e32
YuriiTrofimenko <tyaa@ukr.net>
2024-01-03 15:40:27 +0300

    инициализация базы данных: роли

--- src/main/java/org/tyaa/training/current/server/ServerApplication.java
+++ src/main/java/org/tyaa/training/current/server/ServerApplication.java
@@ -1,7 +1,14 @@
 package org.tyaa.training.current.server;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.tyaa.training.current.server.entities.RoleEntity;
+import org.tyaa.training.current.server.repositories.RoleRepository;
+
+import java.util.List;
 
 /**
  * Главный класс приложения
@@ -9,8 +16,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServerApplication {
 
+	@Value("${custom.init-data.roles}")
+	private List<String> roles;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	/**
+	 * Инициализация БД
+	 * */
+	@Bean
+	public CommandLineRunner initData (RoleRepository roleRepository) {
+		return args -> {
+			roleRepository.truncateTable();
+			roleRepository.save(RoleEntity.builder().name("ROLE_ADMIN").build());
+			roleRepository.save(RoleEntity.builder().name("ROLE_CUSTOMER").build());
+		};
+	}
 }
--- src/main/java/org/tyaa/training/current/server/repositories/RoleRepository.java
+++ src/main/java/org/tyaa/training/current/server/repositories/RoleRepository.java
@@ -1,6 +1,9 @@
 package org.tyaa.training.current.server.repositories;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.tyaa.training.current.server.entities.RoleEntity;
 
@@ -9,5 +12,8 @@ import org.tyaa.training.current.server.entities.RoleEntity;
  * */
 @Repository
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
-
+    @Modifying
+    @Transactional
+    @Query(value = "TRUNCATE TABLE Roles RESTART IDENTITY", nativeQuery = true)
+    void truncateTable();
 }
